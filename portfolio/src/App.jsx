ff10ae5f48c503ba150f846a2f97539f9cfb2d65
sydnirose <salexandra.rose@gmail.com>
2024-04-29 19:18:25 -0500

    update routes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,9 @@
 import { Link, Route, Routes } from 'react-router-dom'
 import { Home } from './views/Home'
 import { About } from './views/About'
-// import { ProjectOne } from './views/ProjectOne'
-// import { ProjectTwo } from './views/ProjectTwo'
-// import { ProjectThree } from './views/ProjectThree'
+import { ProjectOne } from './views/ProjectOne'
+import { ProjectTwo } from './views/ProjectTwo'
+import { ProjectThree } from './views/ProjectThree'
 import './App.css'
 
 function App() {
@@ -21,17 +21,17 @@ function App() {
       </nav>
 
     {/* Projects */}
-      {/* <h2><Link to="/projectone">Project One</Link></h2>
+      <h2><Link to="/projectone">Project One</Link></h2>
       <h2><Link to="/projectone">Project Two</Link></h2>
-      <h2><Link to="/projectone">Project Three</Link></h2> */}
+      <h2><Link to="/projectone">Project Three</Link></h2>
 
     {/* Routes */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        {/* <Route path="/projectone" element={<ProjectOne />} />
+        <Route path="/projectone" element={<ProjectOne />} />
         <Route path="/projecttwo" element={<ProjectTwo />} />
-        <Route path="/projectthree" element={<ProjectThree />} /> */}
+        <Route path="/projectthree" element={<ProjectThree />} />
       </Routes>
     </>
   )
