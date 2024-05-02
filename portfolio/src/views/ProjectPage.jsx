304030c4f39e9a0e9b6df08528045edfea106fca
sydnirose <salexandra.rose@gmail.com>
2024-05-01 21:11:41 -0500

    new pages

--- portfolio/src/views/ProjectPage.jsx
+++ portfolio/src/views/ProjectPage.jsx
@@ -8,6 +8,22 @@ export function ProjectPage({data}) {
     console.log("project", selectedProject);
     return(
         <>
+        {/* Header */}
+        <div className="head">
+            <header>
+                <h1>Sydni Rose</h1>
+            </header>
+
+        {/* Navigation */}
+            <nav>
+                <ul>
+                    <li>Projects</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+
         <div className="productCard">
             <h1>{selectedProject.projectTitle}</h1>
             <p>{selectedProject.skillsUsed}</p>
@@ -18,7 +34,18 @@ export function ProjectPage({data}) {
             <h1>{selectedProject.processTitle}</h1>
             <p>{selectedProject.processDescription}</p>
             <img src={selectedProject.img1} alt={selectedProject.projectTitle} />
+            <img src={selectedProject.img2} alt={selectedProject.projectTitle} />
+            <img src={selectedProject.img3} alt={selectedProject.projectTitle} />
+            <img src={selectedProject.img4} alt={selectedProject.projectTitle} />
         </div>
+
+
+        {/* Footer */}
+        <footer>
+            <div className="foot">
+                <p>This is the footer, will have email and linkedin</p>
+             </div>
+        </footer>
         </>
     )
 }
